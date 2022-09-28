aeb0bc2ae83129476907a625c2eea40743cf9340
YerayJ <114072332+YerayJ@users.noreply.github.com>
2022-09-28 14:42:41 +0200

    Add files via upload

---
+++ EjerciciosBasico2/src/ejerciciosbasicos2/Ejercicio4.java
@@ -0,0 +1,43 @@
+package ejerciciosbasicos2;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	/*
+	 * Diseñar un algoritmo que nos indique si podemos salir a la calle. Existen
+	 * aspectos que influirán en esta decisión: solo podremos salir a la calle si no
+	 * está lloviendo y hemos finalizado nuestras tareas. Existe una opción en la
+	 * que, indistintamente de lo anterior, podremos salir a la calle: el hecho de
+	 * tener que ir a la biblioteca. Solicitar al usuario (mediante un booleano) si
+	 * llueve, si ha finalizado las tareas y si necesita ir a la biblioteca. El
+	 * algoritmo debe mostrar mediante un booleano (true o false) si es posible que
+	 * se le otorgue permiso para salir a la calle.
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		//Creamos las variables
+		boolean lluvia,tarea,biblioteca,algoritmo1,algoritmo2;
+		//Creamos un escaner para añadir datos
+		Scanner key = new Scanner(System.in);
+		//Pedimos los datos al usuario
+		System.out.println("Responda con true o false");
+		System.out.println("Dígame si va a llover:");
+		lluvia = key.nextBoolean();
+		System.out.println("Dígame si ha hecho las tareas: ");
+		tarea = key.nextBoolean();
+		System.out.println("Dígame va a la biblioteca:");
+		biblioteca= key.nextBoolean();
+	
+		//Creamos el algoritmo con operadores ternarios.
+		//Si no llueve y tiene la tarea hecha sale de casa, o si tiene que ir a la biblioteca.
+		algoritmo1 = (lluvia==false && tarea==true) || biblioteca; 
+		//Imprimimos el resultado
+		System.out.println("¿Puede salir? " + (algoritmo1 ? "Sí" : "No"));
+		//Cerramos el escaner
+		key.close();
+	
+	}
+
+}
---
+++ EjerciciosBasico2/src/ejerciciosbasicos2/Ejercicio5.java
@@ -0,0 +1,40 @@
+package ejerciciosbasicos2;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	// Escribir un programa que solicite las notas del primer, segundo y tercer
+	// trimestre (notas enteras que se solicitarán al usuario). El programa debe
+	// mostrar la nota media del curso como se utiliza en el boletín de
+	// calificaciones (solo la parte entera) y como se usa en el expediente
+	// académico (con decimales).
+
+	public static void main(String[] args) {
+		//Creamos las variables
+		int notaPrimera,notaSegunda,notaTercera ;
+		double suma;
+		//Creamos el escaner
+		Scanner key = new Scanner(System.in);
+		//Pedimos las notas al usuario
+		System.out.println("Dame las tres notas como números enteros");
+		System.out.println("Primer Trimestre: ");
+		notaPrimera = key.nextInt();
+		System.out.println("Segundo Trimestre: ");
+		notaSegunda = key.nextInt();
+		System.out.println("Tercer Trimestre: ");
+		notaTercera = key.nextInt();
+		//Hacemos el promedio
+		suma = notaPrimera + notaSegunda + notaTercera;
+		double promedio = suma/3;
+		
+		//Imprimimos el resultado
+		System.out.println("NOTAS");
+		System.out.println("La nota del primer trimestre es " + notaPrimera);
+		System.out.println("La nota del segundo trimestre es " + notaSegunda);
+		System.out.println("La nota del tercer trimestre es " + notaTercera);
+		System.out.println("El resultado total del expediente academico es " + promedio);
+		
+	}
+
+}
